be617ce4f5a5d3e23da21d8e376ce6661f6a7757
Ivan Castellanos <me@ivancastellanos.com>
2017-09-30 22:47:10 -0500

    Add header section

---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  #header hello
+</template>
+
+<script>
+export default {
+  name: 'headerSection',
+  data () {
+    return {
+      msg: 'Welcome to Your Aquesome Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped lang="postcss">
+#header{
+  height:100vh;
+  min-height:100vh;
+  background-color: blue
+}
+</style>
