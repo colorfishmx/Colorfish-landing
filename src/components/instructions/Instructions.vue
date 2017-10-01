8c9b8025a68823914f40582d1c187d60676c875f
Ivan Castellanos <me@ivancastellanos.com>
2017-09-30 22:47:57 -0500

    Add instructions section

---
+++ src/components/instructions/Instructions.vue
@@ -0,0 +1,70 @@
+<template lang="pug">
+  div
+    svg(style='display:block; fill:#E7EEF4;', xmlns='http://www.w3.org/2000/svg', width='100%', height='113.39', viewBox='0 0 1291 113.39', preserveAspectRatio='none')
+      path.cls-2(d='M0,25.39s213.5,91.5,529.08,15.16C953.25-62,1291,62.39,1291,62.39v51H0Z')
+
+    section#features_section
+      .row.text-center.gradient-bg
+        .col-sm-8.col-sm-offset-2.text-center
+          h2.clrfsh_blue ¿Cómo funciona?
+          p.clrfsh_blue
+            | Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra orci ut.
+      .container(v-for="item in items")
+        step(:title="item.title", :desc="item.desc", :img="item.img", :even="isEven=!isEven")
+</template>
+
+<script>
+import Step from './Step'
+
+export default {
+  name: 'instructions',
+  components: {
+    Step
+  },
+  data () {
+    return {
+      isEven: false,
+      items: [
+        { title: `Diseño gráfico en tu proyecto`,
+          desc: `Implementamos un diseño gráfico agradable en todos nuestros proyectos y para mas proyectos.`,
+          img: `https://www.pleo.io/1ddf251deac30528f81a016ff77c28b6.png`},
+        { title: `Google analytics`,
+          desc: `Analiza todo tu tráfico y aprovecha los datos para la toma de decisiones en tu negocio.`,
+          img: `https://www.pleo.io/1ddf251deac30528f81a016ff77c28b6.png`},
+        { title: `Search Engine Optimization`,
+          desc: `Posiciónate en los primeros lugares en google y otros buscadores con el plan profesional.`,
+          img: `https://www.pleo.io/1ddf251deac30528f81a016ff77c28b6.png`},
+        { title: `Google analytics`,
+          desc: `Analiza todo tu tráfico y aprovecha los datos para la toma de decisiones en tu negocio.`,
+          img: `https://www.pleo.io/1ddf251deac30528f81a016ff77c28b6.png`}
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="postcss">
+#features_section{
+  background: #fff;
+  margin: auto;
+  padding: 0;
+}
+
+.gradient-bg{
+  background: linear-gradient(to bottom, #E7EEF4, #FFFFFF);
+  padding-top: 50px;
+  padding-bottom: 180px;
+}
+
+h2{
+  color: #292C3F;
+  font-size: 2.3em;
+}
+
+p{
+  color: ;
+  font-size: 1.0em;
+  font-weight: 400;
+}
+</style>
---
+++ src/components/instructions/Step.vue
@@ -0,0 +1,72 @@
+<template lang="pug">
+  .wrapper
+      .row.vcenter(v-if="even")
+        .col-md-5.offset-1.img
+          .img(:style="{ backgroundImage: `url('${img}')` }")
+        .col-md-4.push-2
+          h2 {{ title }}
+          p {{ desc }}
+          a.btn.btn-feat.btn-lg.scroll(href='#planes')
+            span Planes
+
+      .row.vcenter(v-else)
+        .col-md-4.offset-2
+          h2 {{ title }}
+          p {{ desc }}
+          a.btn.btn-feat.btn-lg.scroll(href='#planes')
+            span Planes
+        .col-md-5.push-1.img
+          .img(:style="{ backgroundImage: `url('${img}')` }")
+
+      hr.one(v-if="even")
+      hr.two(v-else)
+</template>
+
+<script>
+export default {
+  name: 'step',
+  props: ['title', 'desc', 'img', 'even'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="postcss">
+.wrapper{
+  margin: 0;
+}
+
+.vcenter{
+  display: flex;
+  align-items: center;
+}
+
+.img{
+  height: 400px;
+  width: 400px;
+  background-repeat:no-repeat;
+  background-size:contain;
+  background-position:center;
+}
+
+hr.one{
+  display:block;
+  border:0px;
+  height:140px;
+  background-repeat: no-repeat;
+  background-position: center;
+  margin:0px;
+}
+
+hr.two{
+  display:block;
+  border:0px;
+  height:140px;
+  background-repeat: no-repeat;
+  background-position: center;
+  margin:0px;
+}
+</style>
