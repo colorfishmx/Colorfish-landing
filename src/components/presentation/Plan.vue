0b9ed49e14c0be4934f3b501623093facf654a1f
Ivan Castellanos <me@ivancastellanos.com>
2017-09-30 22:47:40 -0500

    Add presentation section

---
+++ src/components/presentation/Plan.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  #profesional-section.main-feature.main-feature-3
+    .container.reorder
+      .row
+        .col-lg-6.col-lg-offset-0.col-md-6.col-md-offset-0.col-sm-10.col-sm-offset-1.col-xs-10.col-xs-offset-1.featureimage3.item1
+          img.Twox(src='img/feat-1.png', width='100%', height='auto')
+        .col-lg-5.col-lg-offset-1.col-md-5.col-md-offset-1.col-xs-10.col-xs-offset-1.item2
+          h3 {{ name }}
+          h2 {{ title }}
+          ul.top-three-features(v-for="advantage in advantages")
+            li
+              p.one-of-the-features {{ advantage }}
+          .pad-sm
+            a.col-xs-12.col-sm-6.btn-aside(href='#') ver ejemplos
+            a.col-xs-12.col-sm-6.btn-primary.sunny.scroll(href='#planes') Ver precio
+</template>
+
+<script>
+export default {
+  name: 'plan',
+  props: ['name', 'title', 'advantages'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="postcss">
+
+</style>
---
+++ src/components/presentation/Presentation.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+  #presentacion
+    div(v-for='plan in plans')
+      Plan(:name='plan.name', :title='plan.title', :advantages='plan.advantages')
+</template>
+
+<script>
+import Plan from './Plan'
+
+export default {
+  name: 'presentation',
+  components: {
+    Plan
+  },
+  data () {
+    return {
+      plans: [
+        {
+          name: `Plan Startup`,
+          title: `Empower your team, while staying in control`,
+          advantages: [
+            `Follow all purchases, categorized with the right receipts`,
+            `Set custom tags to match the accounting procedures`,
+            `Integrate and sync Pleo with your accounting system`
+          ]
+        },
+        {
+          name: `Plan PyME`,
+          title: `No more lost receipts and tedious paperwork`,
+          advantages: [
+            `Real-time notifications when a Pleo card is used`,
+            `Easily capture and match receipts with the Pleo app`,
+            `Automatic categorization of all purchases`
+          ]
+        },
+        {
+          name: `Plan Profesional`,
+          title: `See where the company money is going in real time`,
+          advantages: [
+            `Follow all purchases, categorized with the right receipts`,
+            `Set custom tags to match the accounting procedures`,
+            `Integrate and sync Pleo with your accounting system`
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="postcss">
+
+</style>
