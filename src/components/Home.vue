12ad2bfd114c768f333ceaf6851d0bf938918107
Ivan Castellanos <me@ivancastellanos.com>
2017-10-05 21:29:15 -0500

    Remove random test bottom products

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,12 +4,6 @@
     presentation
     instructions
     prices
-    div(class="products")
-        div(v-for="plan in plans", class="product")
-            p {{ plan.name }}
-            p {{ plan.quick_elevator_pitch }}
-            p {{ plan.price.regular}}
-            p(v-for="feature in plan.features") {{ feature }}
 </template>
 
 <script>
@@ -25,11 +19,6 @@ export default {
     Presentation,
     Instructions,
     Prices
-  },
-  computed: {
-    plans(){
-      return this.$store.state.products
-    }
   }
 }
 </script>
